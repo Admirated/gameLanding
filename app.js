be311fe375cd9159a9930f3b49d54029adfc9fb2
Vladimir <vovanebozhinsky@gmail.com>
2021-12-19 23:13:17 +0200

    finish

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
 
 window.addEventListener('DOMContentLoaded', () => {
+
+   const sections = document.querySelectorAll('section');
    //menu
    const burger = document.querySelector('.menu__icon'),
       menu = document.querySelector('.header__navigation'),
-      navItems = menu.querySelectorAll('.navigation-item');
+      navItems = document.querySelectorAll('.navigation-item');
 
    burger.addEventListener('click', () => {
       menu.classList.toggle('_active');
@@ -18,6 +20,17 @@ window.addEventListener('DOMContentLoaded', () => {
    function setActiveLink(e) {
       document.querySelector('.navigation-item._active').classList.remove('_active');
       this.classList.add('_active');
+      const section = this.dataset.section;
+      for (const sect of sections) {
+         if (sect.dataset.section != section) {
+            if (!sect.classList.contains('hide')) {
+               sect.classList.toggle('hide');
+            }
+         } else if (sect.classList.contains('hide')) {
+            sect.classList.remove('hide');
+            document.querySelector(`.menu li[data-section="${section}"]`).classList.add('_active');
+         }
+      }
    }
 
    new Swiper('.card-slider', {
@@ -49,4 +62,5 @@ window.addEventListener('DOMContentLoaded', () => {
       slidesPerView: 1,
       touchRatio: 0
    });
+
 })
